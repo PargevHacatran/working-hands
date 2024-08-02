1e512a57e3bc41881cff04e6164151f7477dc18c
PargevHacatran <117037858+PargevHacatran@users.noreply.github.com>
2024-08-02 17:04:28 +0300

    Refactor.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -2,17 +2,14 @@ import json
 
 from redis.asyncio import Redis
 
-
 async def get_value(redis: Redis, key: str):
     result = await redis.get(key)
     if result is not None:
         return json.loads(result)
     return result
 
-
 async def set_value(redis: Redis, key: bytes | str | memoryview, value: int | str | list | dict):
     await redis.set(key, json.dumps(value))
-
 
 async def delete_value(redis: Redis, key: str):
     await redis.delete(key)
