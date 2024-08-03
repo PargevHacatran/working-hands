4ced2d2c14fb97bf4ea442f00cbf2e701c6a2ad2
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-08-03 08:27:58 +0300

    change User pydantic model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    login: Annotated[str, Field(default="email@example.com")]
-    role: Annotated[str, Field(default="worker")]
-    phone: Annotated[str, Field(default="9123456789")]
+    email: Annotated[str, Field(default="email@example.com")]
+    tel: Annotated[str, Field(default="9123456789")]
     name: Annotated[str, Field(default="Иванов Иван")]
     position: Annotated[str, Field(default="Плиточник", description="Должность")]
-    balance: int
+    password: str
+
