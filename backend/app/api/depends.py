365f8cb47143e91dd8870e8d95bbe4ecc32d2081
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-08-03 08:10:01 +0300

    change url redis

--- backend/app/api/depends.py
+++ backend/app/api/depends.py
@@ -6,7 +6,7 @@ from redis.asyncio import Redis
 
 
 def get_redis() -> Redis:
-    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    redis = aioredis.from_url("redis://red-cqmrmjlds78s7393dg9g:6379", encoding="utf8", decode_responses=True)
     return redis
 
 
