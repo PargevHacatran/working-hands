0afe919d4e215ba7cee37f1048d107b640f1a806
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-08-03 07:46:58 +0300

    add get-random-code path

--- backend/app/api/routes/authorization.py
+++ backend/app/api/routes/authorization.py
@@ -1,7 +1,9 @@
 from typing import Annotated
+from fastapi import APIRouter, Body, HTTPException
+from starlette import status
 
-from fastapi import APIRouter, Body
-
+from app.utils.generate_random_code import generate_random_code
+from app.utils.send_random_code import generate_text_message, send_random_code
 
 router = APIRouter(
     prefix="/authorization",
@@ -9,6 +11,13 @@ router = APIRouter(
 )
 
 
-@router.post("/set-code")
-def set_code_authorization(code: Annotated[str, Body()]):
-    pass
+@router.post("/get-random-code")
+def get_code_authorization(number: Annotated[str, Body()]):
+    random_code = generate_random_code(code_size=4)
+    text = generate_text_message(random_code=random_code)
+
+    request = send_random_code(destination=number, text=text)
+    if request.status_code == 200:
+        return {"random_code": random_code}
+    else:
+        raise HTTPException(status_code=500, detail=status.HTTP_500_INTERNAL_SERVER_ERROR)
