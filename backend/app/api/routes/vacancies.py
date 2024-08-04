f9cf22fcdcdbff585e7d07ccd4511f0d1c0d9dfc
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-08-04 07:49:33 +0300

    Revert "fix"
    This reverts commit f0e9f4a3e553388748e1b01f93c4b1e1090cefaf.

--- backend/app/api/routes/vacancies.py
+++ backend/app/api/routes/vacancies.py
@@ -58,6 +58,6 @@ async def update_responses(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="vacancy_id not exist")
 
 
-@router.delete("/delete-vacancy/")
-async def delete_vacancy_by_id(vacancy_id: Annotated[str, Body()], redis: RedisDeps):
+@router.delete("/delete-vacancy/{vacancy_id}")
+async def delete_vacancy_by_id(vacancy_id: Annotated[str, Path()], redis: RedisDeps):
     await delete_value(redis, vacancy_id)
