2d900eecb25e91dbb871d6809acc7332e394452d
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-08-04 09:38:44 +0300

    add get-all-users query

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Body, Path
 
 from app.api.depends import RedisDeps
-from app.crud import set_value, get_value, delete_value
+from app.crud import set_value, get_value, delete_value, get_all_values
 from app.models.user import User
 
 
@@ -16,6 +16,16 @@ router = APIRouter(
 async def set_user(value: Annotated[User, Body()], redis: RedisDeps):
     await set_value(redis, value.email, dict(value))
     return {"result": value}
+
+
+@router.get("/get-all-users")
+async def get_vacancies(redis: RedisDeps):
+    result = []
+    all_values = await get_all_values(redis)
+    for value in all_values:
+        if value.get("email") is not None:
+            result.append(value)
+    return result
 
 
 @router.get("/get-user/{email}")
